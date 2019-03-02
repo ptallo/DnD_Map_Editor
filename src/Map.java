084e5ac2799368d79264dba50a4463c608eb87d0
Philip Tallo <tallopt@mail.uc.edu>
2019-03-02 13:03:05 -0500

    initial architecture

---
+++ src/Map.java
@@ -0,0 +1,16 @@
+import javafx.scene.canvas.GraphicsContext;
+
+import java.util.ArrayList;
+
+public class Map {
+
+    private ArrayList<ArrayList<Tile>> tileMatrix = new ArrayList<>();
+
+    public Map() {
+        
+    }
+
+    public void draw(GraphicsContext gc) {
+
+    }
+}
