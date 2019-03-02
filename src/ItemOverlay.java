4d2786bbe9d0d7f116a1e60d3bccc8e3494f4b5b
Christopher Ochs <ochscs@mail.uc.edu>
2019-03-02 16:19:17 -0500

    Added a lot of stuff

--- src/ItemOverlay.java
+++ src/ItemOverlay.java
@@ -1,4 +1,55 @@
-import javafx.scene.control.ScrollPane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class ItemOverlay extends ScrollPane {
+
+    private MenuBar menuBar = new MenuBar();
+    private GridPane gridPane;
+
+    public ItemOverlay(Stage primaryStage, GridPane grid) {
+        createMenuBar(primaryStage);
+        gridPane = grid;
+    }
+
+    void createMenuBar(Stage primaryStage) {
+        // Create menus
+        Menu file = new Menu("File");
+        Menu edit = new Menu("Edit");
+        Menu view = new Menu("View");
+
+        // Create submenus
+        ToggleGroup toggleGroup = new ToggleGroup();
+        RadioMenuItem editorMode = new RadioMenuItem("Editor Mode");
+        RadioMenuItem gameMode = new RadioMenuItem("Game Mode");
+        toggleGroup.getToggles().addAll(editorMode, gameMode);
+
+        editorMode.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                setEditorMode();
+            }
+        });
+
+        // Add submenus to parent button
+        view.getItems().addAll(gameMode, editorMode);
+
+        // Add to menu bar
+        menuBar.getMenus().addAll(file, edit, view);
+        menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
+    }
+
+    void createEditorMenu() {
+        RadioButton dirtButton = new RadioButton("Dirt");
+        RadioButton grassButton = new RadioButton("Grass");
+    }
+
+    void setEditorMode() {
+    }
+
+    MenuBar getMenuBar() {
+        return menuBar;
+    }
 }
