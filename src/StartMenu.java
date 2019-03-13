2400df84e95dbd03e6e3b4b1e6fa139e25183fc8
Philip Tallo <tallopt@mail.uc.edu>
2019-03-13 10:15:19 -0400

    added load button functionality

--- src/StartMenu.java
+++ src/StartMenu.java
@@ -1,12 +1,17 @@
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 
 public class StartMenu extends VBox {
@@ -33,7 +38,21 @@ public class StartMenu extends VBox {
         Button loadMapButton = new Button("Load Map");
         loadMapButton.setMaxWidth(Double.MAX_VALUE);
         loadMapButton.addEventFilter(MouseEvent.MOUSE_CLICKED, event ->  {
-            System.out.println("load map button");
+            FileChooser chooser = new FileChooser();
+            chooser.setTitle("Load Map");
+            File savedFile = chooser.showOpenDialog(primaryStage);
+
+            if (savedFile != null) {
+                DndEditor editor = new DndEditor(primaryStage);
+                try {
+                    editor.getCanvasHandler().getCanvasMap().loadMapFromText(savedFile);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                editor.getCanvasHandler().draw();
+                primaryStage.setScene(new Scene(editor));
+            }
+
         });
 
         getChildren().addAll(menuTitle, newMapButton, loadMapButton);
